79b1b4b96b493258eb850406aab999e43bfe3755
Markus Wedler <markuswedler8@gmail.com>
2023-11-02 13:51:30 +1000

    Add a sertificate

--- components/about/Sertificates.tsx
+++ components/about/Sertificates.tsx
@@ -1,6 +1,7 @@
 import img_1 from "@/images/about/sertificates/1.png"
 import img_2 from "@/images/about/sertificates/2.png"
 import img_3 from "@/images/about/sertificates/3.png"
+import img_4 from "@/images/about/sertificates/4.png"
 import Image from "next/image"
 
 export default ({ json }: { json: { [key: string]: string } }) => {
@@ -8,15 +9,11 @@ export default ({ json }: { json: { [key: string]: string } }) => {
     <section className="sertificates wrapper">
       <div className="title" dangerouslySetInnerHTML={{ __html: json.title }} />
       <div className="grid">
-          <div className="img-cont">
-            <Image src={img_1} alt="" placeholder="blur" />
-          </div>
-          <div className="img-cont">
-            <Image src={img_2} alt="" placeholder="blur" />
-          </div>
-          <div className="img-cont">
-            <Image src={img_3} alt="" placeholder="blur" />
-          </div>
+        { [img_1, img_2, img_3, img_4].map(img => (
+            <div className="img-cont">
+              <Image src={img} alt="" placeholder="blur" />
+            </div>
+        )) }
       </div>
     </section>
   )
